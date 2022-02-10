bdb64e2b5bef304996066495768965d55b998277
IssamSahraoui <mail@issamsahraoui.me>
2022-02-10 09:42:07 +0100

    updating_Gui_elements

--- src/main/java/io/beyn/spell/FXMLDocumentController.java
+++ src/main/java/io/beyn/spell/FXMLDocumentController.java
@@ -24,7 +24,6 @@ import javafx.scene.text.TextFlow;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
